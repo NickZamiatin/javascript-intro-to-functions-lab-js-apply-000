aaf1380533d93c83d8a9ea0c1e3377371e0a9b41
MYKYTA ZAMIATIN <4406015@gmail.com>
2017-12-07 20:58:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,13 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-}
-
-function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
-    return "YES INDEED!"
+      return "YES INDEED!"
+    }
+
+    if (string === "I love you, Grandma.") {
+      return "I love you, too."
+    }
+
+    return "Are you eating enough?"
   }
-}
